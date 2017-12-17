471cdfffd143e4df7453145d9e5f30cd0436f985
Seán Bruton <sbruton@tcd.ie>
2017-12-17 21:49:34 +0000

    Revert to no condition variable

--- src/Recorder.h
+++ src/Recorder.h
@@ -35,9 +35,12 @@ public:
         size_t id = 0;
         for(auto grabber : grabbers) {
             boost::shared_ptr<RecorderNode <PointT>> new_node(new RecorderNode<PointT>(grabber, id, buffer_size));
-            boost::function<void (void)> start_callback =
-                    boost::bind(&RecorderNode<PointT>::start, new_node);
-            connections_.push_back(connectStart(start_callback));
+
+            new_node->start ();
+//            boost::function<void (void)> start_callback =
+//                    boost::bind(&RecorderNode<PointT>::start, new_node);
+//            connections_.push_back(connectStart(start_callback));
+
             boost::function<void (void)> stop_callback = boost::bind(&RecorderNode<PointT>::stop, new_node);
             connections_.push_back(connectStop(stop_callback));
             nodes_.push_back(new_node);
